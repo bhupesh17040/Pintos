6e6a61f9bc9639b2f43960bb07f5a07f6e95a1e7
Bhupesh S Kainth <43728552+bhupesh17040@users.noreply.github.com>
2019-02-08 23:54:10 +0530

    Update synch.h

--- src/threads/synch.h
+++ src/threads/synch.h
@@ -22,6 +22,12 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    /* Changes were made here */
+    // Maximum priority of lock
+    int pri_max; 
+    int primin;
+    // list_elem is an in-built function in (list.h) library.
+    struct list_elem elem;
   };
 
 void lock_init (struct lock *);
@@ -29,6 +35,9 @@ void lock_acquire (struct lock *);
 bool lock_try_acquire (struct lock *);
 void lock_release (struct lock *);
 bool lock_held_by_current_thread (const struct lock *);
+/*Changes were made here */
+// Created a boolean function to check priorities of lock(H,M,L)
+bool prioritylockcheck(const struct list_elem*f1,const struct list_elem*f2,void *aux);
 
 /* Condition variable. */
 struct condition 
